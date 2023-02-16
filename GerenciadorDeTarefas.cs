e81a2e907a02cab81e8b5b2d52872bd03d8e2471
ejunior27 <51997209+ejunior27@users.noreply.github.com>
2023-02-16 09:14:46 -0300

    first commit

---
+++ GerenciadorDeTarefas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_TP1.Aula7
+{
+    public class GerenciadorDeTarefas
+    {
+        public List<string>? tasks { get; set; }
+
+        public GerenciadorDeTarefas()
+        {
+            tasks = new();
+        }
+
+        public string RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                string task = tasks[index];
+                tasks.RemoveAt(index);
+                return $"Tarefa '{task}' removida com sucesso.";
+            }            
+            
+            return "Índice inválido. Tente novamente.";            
+        }
+
+        public List<string> ListTasks()
+        {
+            return tasks;
+        }
+
+        public string AddTask(string task)
+        {
+            tasks.Add(task);
+            return $"Tarefa '{task}' adicionada com sucesso.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+
+namespace CS_TP1.Aula7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gerenciador de Tarefas
+            //-Adicionar tarefa
+            //-Listar tarefas
+            //-Remover tarefa
+            var gerenciador = new GerenciadorDeTarefas();
+
+            while (true)
+            {
+                Console.WriteLine("Gerenciador de Tarefas");
+                Console.WriteLine("1 - Adicionar tarefa");
+                Console.WriteLine("2 - Listar tarefas");
+                Console.WriteLine("3 - Remover tarefa");
+                Console.WriteLine("4 - Sair");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Digite a tarefa a ser adicionada:");
+                        var retorno = gerenciador.AddTask(Console.ReadLine());
+                        break;
+                    case 2:
+                        var lista = gerenciador.ListTasks();
+                        Console.WriteLine("Lista de tarefas:");
+                        foreach (string task in lista)
+                        {
+                            Console.WriteLine($"- {task}");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite o índice da tarefa a ser removida:");
+                        var retornoRemove = gerenciador.RemoveTask(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+        }        
+    }
+}
